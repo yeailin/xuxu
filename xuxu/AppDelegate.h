b0cb005073e5555c243fd0cd0781259702759664
caoye <302995050@qq.com>
2019-05-22 08:36:52 +0800

    Initial Commit

---
+++ xuxu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xuxu
+//
+//  Created by 曹野 on 2019/5/22.
+//  Copyright © 2019 曹野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xuxu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xuxu
+//
+//  Created by 曹野 on 2019/5/22.
+//  Copyright © 2019 曹野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
